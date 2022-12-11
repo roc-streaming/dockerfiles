e6014b92a0f61b5d475a3e8f8d02d5927317ba4d
Andrew Petelin <adrianopol@rbox.me>
2022-12-11 16:57:36 +0300

    scripts refactoring

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,7 +1,8 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
 set -euo pipefail
 
-action="$1"
+action="${1:?action needed}"
 
 root_dir="$(cd $(dirname "$0")/.. && pwd)"
 cd "$root_dir"
@@ -12,25 +13,25 @@ num_images="$(echo $images | tr ' ' '\n' | wc -l)"
 cur_image=1
 
 for image_spec in $images; do
-    image_dir=$(dirname $image_spec)
-    image_name=$(basename $image_dir)
+    image_dir="$(dirname "$image_spec")"
+    image_name="$(basename "$image_dir")"
 
     printf '%s \033[1;34m[%d/%d] processing image %s\033[0m\n' \
-           "--" $cur_image $num_images $image_name
+           "--" "$cur_image" "$num_images" "$image_name"
 
     cd "$root_dir/$image_dir"
 
-    case $action in
-        --build)
-            "$root_dir/scripts/build.sh"
-            ;;
-        --push)
-            "$root_dir/scripts/push.sh"
-            ;;
+    case "$action" in
+    (--build)
+        "$root_dir/scripts/build.sh"
+        ;;
+    (--push)
+        "$root_dir/scripts/push.sh"
+        ;;
     esac
 
     (( cur_image++ ))
 done
 
 printf '%s \033[1;34msuccessfully processed %d images\033[0m\n' \
-       "--" $num_images
+       "--" "$num_images"
